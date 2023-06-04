de8f77330c5c4679a48b80bef9a4968973277930
facundo cahue <fcahue@frba.utn.edu.ar>
2023-06-03 21:01:07 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 int main() {
-    long double i = 1;                              //inserto las variables       
-    long double pi = 4;                             //a pi lo multiplico por 4 previamente ya que la serie de Leibniz da π/4
-    long double diferencia = pi - 3.141592;
-    long double requisito = 0.000001;               //indicamos el requisito pedido
-
-    while (diferencia > requisito) {                //Calculamos la diferencia con el valor deseado
-
-         pi += (-4 /(4*i - 1)) + (4 /(4*i + 1));    //uso la Serie de Leibniz adaptada para c++
-        ++i;
+    long double i = 1;                               //inserto las variables
+    long double pi = 4;                              //pi lo multiplico por 4 porque la serie de Leibniz da π/4
+    long double diferencia;
+    long double requisito = 9e-7;
+    
+    while (true) {
+         pi += (-4 /(4*i - 1)) + (4 /(4*i + 1));     //uso la Serie de Leibniz adaptada para c++       
+         diferencia = pi - 3.141592;
+         if(diferencia<requisito){                   //termino el siclo cuando el valor nro 6 decimal no varia
+            break;
+         }
+         ++i;
     }
-
-    cout << setprecision(7) << fixed << pi << endl;
+    cout << setprecision(7) << fixed << pi << endl;  //uso precision 7 ya que el siguiente numero de 2 es mayor a 5 y lo redondea para arriba
     return 0;
 }
